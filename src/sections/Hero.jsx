0c257d14e43fa1237ba1c1722e27365fce6c1c1f
Amit Sarkar <amitsarkar6124@gmail.com>
2026-01-29 12:15:37 +0530

    fix build issues

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,7 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import HeroText from "../components/HeroText";
 import { Particles } from "../components/Particles";
-import ParallaxBackground from "../components/ParallaxBackground";
+import ParallaxBackground from "../components/parallaxBackground";
 import { Float } from "@react-three/drei";
 import { useMediaQuery } from "react-responsive";
 import { easing } from "maath";
